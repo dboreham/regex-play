f3e9675f7d587605469729c644594ef15104d32d
David Boreham <david@bozemanpass.com>
2022-07-29 18:59:39 -0600

    Done

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,7 +1,18 @@
 
 import { linkReplacer } from "./main"
 
-test('Link is unchanged', () => {
-    const testLink = "/foo"
-    expect(linkReplacer(testLink)).toBe(testLink);
+test.each`
+  original  | transformed
+  ${'./foo.md'}                   | ${'./foo'}
+  ${'./foo'}                      | ${'./foo'}
+  ${'../foo.md'}                  | ${'../foo'}
+  ${'../bar/foo.md'}              | ${'../bar/foo'}
+  ${'/bar/foo.md'}                | ${'/bar/foo'}
+  ${'/bar/foo.mdsomething'}       | ${'/bar/foo.mdsomething'}
+  ${'./foo.md#anchor'}            | ${'./foo#anchor'}
+  ${'../bar/foo.md#anchor'}       | ${'../bar/foo#anchor'}
+  ${'./bar#anchor'}               | ${'./bar#anchor'}
+  ${'http://host.com/foo/bar.md'} | ${'http://host.com/foo/bar.md'}
+`('transforms $original to $transformed', ({ original, transformed }) => {
+    expect(linkReplacer(original)).toBe(transformed)
 });
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 
-export const linkReplacer = (input: string) => {
-    return input
+export const linkReplacer = (link: string) => {
+    const matcher = new RegExp('^(\./|\.\./|/)(.*?)(.md)(\#.*?)?$')
+    if (matcher.test(link)) {
+      const newLink = link.replace(matcher, "$1$2$4") 
+      // console.log(`Translated ${link} into ${newLink}`)
+      return newLink
+    } else {
+      return link
+    }
 }
